3f3bd17a5fe1c1dd99d013a7bd5b1f7beddeef7b
Your Name <andres.lagerlof@gmail.com>
2023-01-08 11:25:30 +0100

    initial commit

---
+++ assets/data/dates.R
@@ -0,0 +1,16 @@
+
+# Dates for Vårcup 2023 ---------------------------------------------------
+
+
+# Load libraries
+library(tidyverse)
+
+# Create tibble with dates
+tribble(
+  ~no, ~date,
+  1,    2023-01-26,
+  2,    2023-02-23,
+  3,    2023-03-30,
+  4,    2023-04-27,
+  5,    2023-05-25,
+)
